e495ccab05cac9b698a02b542918c4adf0fb16f2
ZyuC <m9t1k3zczc@gmail.com>
2020-04-28 00:15:31 +0900

    パラメータの初期化

--- main.py
+++ main.py
@@ -3,5 +3,10 @@ sys.path.append("scripts")
 from read_mnist import *
 
 class TwoLayerNet:
-    def __init__(self):
+    def __init__(self, input_size, hidden_size, output_size, weight_init_std = 0.01):
+        self.params = {}
+        self.params['W1'] = weight_init_std * np.random.randn(input_size, hidden_size)
+        self.params['b1'] = np.zeros(hidden_size)
+        self.params['W2'] = weight_init_std * np.random.randn(hidden_size, output_size)
+        self.params['b2'] = np.zeros(output_size)
         return
