9f0efe6e1b26f75d268b8cd44599fb2b5209b252
ZyuC <m9t1k3zczc@gmail.com>
2020-04-28 00:57:36 +0900

    交差エントロピー誤差の計算を実装

--- scripts/functions.py
+++ scripts/functions.py
@@ -7,3 +7,7 @@ def softmax(x):
     a = np.max(x)
     exp_x = np.exp(x - a)
     return exp_x / np.sum(exp_x)
+
+def cross_entropy_error(y, t):
+    delta = 1e-7
+    return -np.sum(t * np.log(y + delta))
