3caa4e53b88df14afa80045081d00afb5d752f13
ZyuC <m9t1k3zczc@gmail.com>
2020-04-28 00:41:32 +0900

    推論を行う関数を実装

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import sys
 sys.path.append("scripts")
 from read_mnist import *
+from functions import *
 
 class TwoLayerNet:
     def __init__(self, input_size, hidden_size, output_size, weight_init_std = 0.01):
@@ -10,3 +11,10 @@ class TwoLayerNet:
         self.params['W2'] = weight_init_std * np.random.randn(hidden_size, output_size)
         self.params['b2'] = np.zeros(output_size)
         return
+
+    def predict(self, x):
+        a1 = np.dot(x, self.params['W1']) + self.params['b1']
+        z1 = sigmoid(a1)
+        a2 = np.dot(z1, self.params['W2']) + self.params['b2']
+        y = softmax(a2)
+        return y
