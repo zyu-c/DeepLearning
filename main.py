b20303746714b22243be5ad5547a32552ae16bc9
ZyuC <m9t1k3zczc@gmail.com>
2020-04-29 01:34:33 +0900

    1つのパラメータで1回だけ勾配計算の動作確認

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from functions import *
 
 class TwoLayerNet:
     def __init__(self, input_size, hidden_size, output_size, weight_init_std = 0.01):
+        self.a = 0
         self.params = {}
         self.params['W1'] = weight_init_std * np.random.randn(input_size, hidden_size)
         self.params['b1'] = np.zeros(hidden_size)
@@ -20,6 +21,8 @@ class TwoLayerNet:
         return y
 
     def loss(self, x, t):
+        self.a = self.a + 1
+        print(self.a)
         y = self.predict(x)
         return cross_entropy_error(y, t)
 
@@ -29,11 +32,36 @@ class TwoLayerNet:
         t = np.argmax(t, axis = 1)
         return np.sum(y == t) / float(x.shape[0])
 
-    def numerical_gradient(self, x, t):
+    def gradient(self, x, t):
         grads = {}
-        grads['W1'] = numerical_gradient(self.predict(x), self.params['W1'])
-        grads['b1'] = numerical_gradient(self.predict(x), self.params['b1'])
-        grads['W2'] = numerical_gradient(self.predict(x), self.params['W2'])
-        grads['b2'] = numerical_gradient(self.predict(x), self.params['b2'])
+        grads['W1'] = self.numerical_gradient(self.loss, self.params['W1'], x, t)
+        #grads['b1'] = self.numerical_gradient(self.loss, self.params['b1'], x, t)
+        #grads['W2'] = self.numerical_gradient(self.loss, self.params['W2'], x, t)
+        #grads['b2'] = self.numerical_gradient(self.loss, self.params['b2'], x, t)
 
         return grads
+
+    def numerical_gradient(self, f, a, x, t):
+        h = 1e-4
+        grad = np.zeros_like(a)
+
+        it = np.nditer(a, flags = ['multi_index'])
+        while not it.finished:
+            tmp_val = a[it.multi_index]
+            a[it.multi_index] = tmp_val + h
+            fxh1 = f(x, t)
+
+            a[it.multi_index] = tmp_val - h
+            fxh2 = f(x, t)
+
+            a[it.multi_index] = tmp_val
+            grad[it.multi_index] = (fxh1 - fxh2) / (2 * h)
+            it.iternext()
+        return grad
+
+net = TwoLayerNet(input_size = 784, hidden_size = 100, output_size = 10)
+
+x = np.random.rand(100, 784)
+t = np.random.rand(100, 10)
+grads = net.gradient(x, t)
+print(grads['W1'].shape)
