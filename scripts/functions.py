5447d92f61d41a487564571534bf4b24cebbfe1f
ZyuC <m9t1k3zczc@gmail.com>
2020-04-29 00:44:51 +0900

    数値微分での勾配の計算を実装

--- scripts/functions.py
+++ scripts/functions.py
@@ -11,3 +11,22 @@ def softmax(x):
 def cross_entropy_error(y, t):
     delta = 1e-7
     return -np.sum(t * np.log(y + delta))
+
+def numerical_gradient(f, x):
+    h = 1e-4
+    grad = np.zeros_like(x)
+
+    it = np.nditer(x, frags = ['multi_index'])
+    while not it.finished:
+        tmp_val = x[it.index]
+        x[it.index] = tmp_val + h
+        fxh1 = f(x)
+
+        x[it.index] = tmp_val - h
+        fxh2 = f(x)
+
+        x[it.index] = tmp_val
+        grad[it.index] = (fxh1 - fxh2) / (2 * h)
+        it.iternext()
+
+    return grad        
