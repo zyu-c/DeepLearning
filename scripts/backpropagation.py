62c49f3fcb4a778edc66f257c0cf9a067945dc10
ZyuC <m9t1k3zczc@gmail.com>
2020-04-29 02:22:16 +0900

    シグモイドレイヤの実装

--- scripts/backpropagation.py
+++ scripts/backpropagation.py
@@ -20,3 +20,14 @@ class AddLayer:
 
     def backward(self, dout):
         return dout, dout
+
+class Sigmoid:
+    def __init__(self):
+        self.out = None
+
+    def forward(self, x):
+        self.out = 1 / (1 + np.exp(-x))
+        return self.out
+
+    def backward(self, dout):
+        return dout * self.out * (1.0 - self.out)
